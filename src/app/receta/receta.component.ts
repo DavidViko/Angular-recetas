ca4ba1443eb8b422323f65fb287971aa94d0f7f9
Curso <Curso@IPARTEKAULA.COM>
2018-02-27 13:32:35 +0100

    completar receta

---
+++ src/app/receta/receta.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receta',
+  templateUrl: './receta.component.html',
+  styleUrls: ['./receta.component.scss']
+})
+export class RecetaComponent implements OnInit {
+
+  // atributos
+  nombre: string;
+  imagen:string;
+  descripcion: string;
+  cocinero:string;
+  likes: number;
+  isGlutenFree: boolean;
+  ingredientes: string[];
+
+  constructor() {
+    console.log('RecetaComponent constructor');
+    this.nombre = 'Bokata kalamares';
+    this.cocinero ="Karlos Arguiñano";
+    this.descripcion = 'Bocadillo relleno de calamares rebozados en forma de aro';
+    this.likes = 13;
+    this.imagen = 'https://masdemadrid.com/wp-content/uploads/2017/03/bocadillo-calamares-560x314.jpg';
+    this.isGlutenFree = true;
+    this.ingredientes = ['Calamares','Pan','Salsa Ali-oli','Limón'];
+  }
+    
+  ngOnInit() {
+    console.log('RecetaComponent ngOnInit');
+  }
+
+  sumarLikes(){
+    this.likes++;
+    console.log('Click sumarlike');
+  }
+
+}
