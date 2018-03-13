3debf270eaf03dae0787fe952e8c84c8ffc09359
Curso <Curso@IPARTEKAULA.COM>
2018-03-13 14:26:44 +0100

    Patch tareas

--- src/app/providers/todos.service.ts
+++ src/app/providers/todos.service.ts
@@ -1,20 +1,64 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-
-const END_POINT = 'http://localhost:3000';
+import { Todo } from '../model/todo';
+import { GLOBAL } from '../global';
 
 @Injectable()
 export class TodosService {
+  
 
-  constructor(public http: HttpClient) {
+  constructor( public http: HttpClient) { 
     console.log('TodosService constructor');
   }
 
-  getTodos(): Observable<any> {
-    let url = END_POINT + '/todos?userId=2';
-    console.log(`TodosService getTodos $(url)`);
+
+  getTodos():Observable<any>{
+
+    //let url = END_POINT + '/todos?userId=2';
+    let url = GLOBAL.endpoint + '/todos';
+    console.log(`TodosService getTodos ${url}`);
     return this.http.get(url);
+
+  }
+
+  delete(id){
+    let url = GLOBAL.endpoint + '/todos/'+id;
+    console.log(`TodosService delete ${url}`);
+    return this.http.delete(url);
+  }
+
+post(todo:Todo){
+    let url = GLOBAL.endpoint + '/todos/';
+    console.log(`TodosService put ${url}`);
+
+    let body = {
+                  // "id": todo.id,
+                  "userId": todo.idUser,
+                  "title": todo.title,
+                  "completed": todo.completed    
+                } 
+              
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+
+    return this.http.post( url, body , httpOptions );
+  }
+
+  patch(todo: Todo):Observable<any>{
+    let url = GLOBAL.endpoint + `/todos/${todo.id}`;
+    console.log(`TodosService patch ${url}`);
+    console.log("TodosService patch"+url);
+
+    let body = {                    
+                  "completed": !todo.completed    
+                } 
+              
+    
+    return this.http.patch( url, body );
   }
 
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -11,6 +11,7 @@ import { element } from 'protractor';
 export class TodosComponent implements OnInit {
 
   todos: Todo[];
+  nuevaTarea: string;
 
   constructor(public todosService: TodosService) {
     console.log('TodosComponent constructor');
@@ -32,6 +33,20 @@ export class TodosComponent implements OnInit {
 
   }//final onInit
 
+  cargarTareas() {
+    console.log('TodosComponent cargarTareas');
+    this.todos = [];
+    this.todosService.getTodos().subscribe(
+      resultado => {
+        console.debug('peticion correcta %o', resultado);
+        this.mapeo(resultado);
+      },
+      error => {
+        console.warn('peticion incorrecta %o', error);
+      }
+    );//subscribe
+  }
+
   /**
    * Mapea los Datos en formato Json a TODO que proviene del Servicio Rest
    * @param resul 
@@ -48,21 +63,97 @@ export class TodosComponent implements OnInit {
     });
   }
 
-  clickar(id: number) {
-    this.todos.forEach((element, index) => {
-      if (this.todos[id] == element[index]) {
-        this.todos[id].completed = !this.todos[id].completed;
-        console.log("Se ha modificado algo la tarea %i a %o", id, this.todos[id].completed);
+  clicar(todo: Todo) {
+    console.log('TodosComponent change %o', todo);
+    this.todosService.patch(todo).subscribe(
+      result => {
+        console.log('Tarea modificada con exito %o', result);
+        this.cargarTareas();
+      },
+      error => {
+        alert('No de pudo Modificar la Tarea');
       }
-    });
+    );
+
+    // SIN CONECTARSE AL SERVICIO
+    // this.todos.forEach((elemento, index) => {
+    //   if (elemento.id === todo.id) {
+    //     this.todos[index].completed = !todo.completed;
+    //     return false; //break        
+    //   }
+    // });
   }
 
-  eliminarTarea(id: number) {
-    console.log("Funcion eliminarTarea %i",id);
-    this.todos.forEach((element, index) => {
-      console.log("Funcion eliminarTarea %i",index);
-      if (this.todos[id] === element[index-1]) {
-        this.todos.splice(id,1);
-        console.log("Dentro del if %o",this.todos);
+  // VERSION ALTERNATIVA PASANDO SOLO EL ID
+  // clicar(id: number) { // Le paso solo el id en vez de el objeto entero
+  //   var indice = this.buscarTareaPorId(id);
+  //   this.todos[indice].completed = !this.todos[indice].completed;
+  // }
+
+
+  eliminarTarea(todo: Todo) {
+    console.log("Funcion eliminarTarea %o", todo);
+    this.todosService.delete(todo.id).subscribe(
+      result => {
+        this.cargarTareas();
+      },
+      error => {
+        alert('No de pudo elimiar Tarea');
+      }
+    );
+    /*
+    this.todos.forEach( (t, index) =>{
+      if ( t.id === todo.id ){
+        this.todos.splice(index,1);
+        return false; //break        
+      }
+    });*/
+  };
+
+  // VERSION ALTERNATIVA PASANDO SOLO EL ID
+  eliminarTarea2(id) {
+    const indice = this.buscarTareaPorId(id);
+    this.todos.splice(indice, 1);
+  }
+
+
+  agregarTarea() {
+    console.log('TodosComponent new ');
+    let todo = new Todo(this.nuevaTarea);
+    /*
+    let todo = new Todo(this.nuevaTarea);
+    this.todos.unshift(todo);
+    this.nuevaTarea = "";
+    */
+    this.todosService.post(todo).subscribe(
+      result => {
+        console.log('TodosComponent new %o', result);
+        this.cargarTareas();
+      },
+      error => {
+        alert('No de pudo Crear Nueva Tarea');
+        console.error(error);
+      }
+    );
+  }
+  /**
+   * Función que vale para buscar el objeto dentro del array "todos" por Id.
+   * Devuelve el índice donde se encuentra el objeto buscado dentro del array.
+   * Si se pasa el objeto todo entero desde el HTML no es neceario usar esta función
+   * @param id Id pasado por parametro desde el html para buscar, borrar o modificar 
+   */
+  buscarTareaPorId(id): number {
+    let i = 0;
+    let encontrado = false;
+
+    while (!encontrado) {
+      if (this.todos[i].id === id) {
+        encontrado = true;
+      } else {
+        i++;
       }
     }
+    return i;
+  }
+
+}
